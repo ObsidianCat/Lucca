c489e50f211c6b3d98d1413a5985df04c2b0abda
ObsidianCat <lulala@FEDS-410-I7.tntraffic.local>
2015-07-08 13:42:05 +0300

    Work on content-item page
    I wrote a html markup and started to work on reviews ng directive

---
+++ scripts/vendor/reviewDirective.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lulala on 7/8/2015.
+ */
+angular.module('luccaApp').directive('reviewItem', function(){
+        return {
+            restrict:'E',
+            templateUrl:'html-templates/review-item.html'
+
+        }
+})
