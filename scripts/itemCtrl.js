c0f617a154a54ea19daafacf0beafed8af0b14ab
ObsidianCat <lulala@FEDS-410-I7.tntraffic.local>
2015-07-12 09:54:13 +0300

    Work on review section in content item page

--- scripts/itemCtrl.js
+++ scripts/itemCtrl.js
@@ -1,10 +1,15 @@
 angular.module('luccaApp').controller('itemController', function($scope, $http, $resource, $routeParams, GetData){
     $scope.item;
     $scope.param = 'testItem.json';
-    $scope.active
+    $scope.activeImg = 0;
     $scope.item = GetData.returnedData.getObject({id:$scope.param}).$promise.then(function(data){
         $scope.item = data;
     });
 
+    $scope.setActiveImg = function($index){
+        $scope.activeImg = $index;
+    };
+
+    //$scope.showHideToggler = fucntion(){};
 
 });
