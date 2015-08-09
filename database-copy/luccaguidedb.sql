d56d9d5b06e98fc8d5fbf29feb494822a93a6d14
lula.l <lula.l@convertmedia.com>
2015-08-09 13:34:31 +0300

    Files of databases added for work

---
+++ database-copy/luccaguidedb.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 09, 2015 at 12:33 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `luccaguidedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contentitems`
+--
+
+CREATE TABLE IF NOT EXISTS `contentitems` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `mainTitle` varchar(200) NOT NULL,
+  `subTitle` varchar(200) NOT NULL,
+  `address` text NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
