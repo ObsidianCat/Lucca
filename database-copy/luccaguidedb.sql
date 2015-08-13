9d02bbe9ab9bfcc76347bf6a8614f5d21ef75c79
lula.l <lula.l@convertmedia.com>
2015-08-13 10:42:30 +0300

    Database Updated

--- database-copy/luccaguidedb.sql
+++ database-copy/luccaguidedb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2015 at 12:33 PM
+-- Generation Time: Aug 13, 2015 at 09:41 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,17 +23,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contentitems`
+-- Table structure for table `items`
 --
 
-CREATE TABLE IF NOT EXISTS `contentitems` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `items` (
+  `item_id` int(6) NOT NULL AUTO_INCREMENT,
   `mainTitle` varchar(200) NOT NULL,
   `subTitle` varchar(200) NOT NULL,
   `address` text NOT NULL,
   `description` text NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `review_id` int(6) NOT NULL AUTO_INCREMENT,
+  `author` varchar(200) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `dateofreview` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `dateofvisit` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `rating` int(2) NOT NULL,
+  `message` text NOT NULL,
+  `item_id` int(6) DEFAULT NULL,
+  PRIMARY KEY (`review_id`),
+  KEY `item_id` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
