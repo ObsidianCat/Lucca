380efbfb350cca1a88f68598148b6a23b3ad0d44
lula.l <lula.l@convertmedia.com>
2015-08-16 09:12:21 +0300

    Started work on weather widget

--- scripts/mainCategoryCtrl.js
+++ scripts/mainCategoryCtrl.js
@@ -1,11 +1,26 @@
 /**
  * Created by Aleosha on 06.06.2015.
  */
-angular.module('luccaApp').controller('mainCategoryController', function($scope, $http, $resource, GetData){
+angular.module('luccaApp').controller('mainCategoryController', function($scope, $http, $resource, GetData, WEATHER_URL){
     $scope.categories = "";
 
     $scope.categories = GetData.returnedData.get({res:'mainCategories'}).$promise.then(function(data) {
         $scope.categories = data;
     });
+
+
+    $http.get(WEATHER_URL.F).
+        success(function(data, status, headers, config) {
+            $scope.weather = data;
+            console.log($scope.weather);
+        }).
+        error(function(data, status, headers, config) {
+            // log error
+            // });
+     });
+
+    setWeather = function(weatherData){
+
+    }
 
 });
