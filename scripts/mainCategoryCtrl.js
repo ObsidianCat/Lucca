952fd9e9c553294191f71cca47688aa8e8e89f51
ObsidianCat <opheliaandcat@gmail.com>
2015-08-22 11:56:20 +0300

    Work on weather widget

--- scripts/mainCategoryCtrl.js
+++ scripts/mainCategoryCtrl.js
@@ -1,6 +1,17 @@
 /**
  * Created by Aleosha on 06.06.2015.
  */
+//var weatherDates = [];
+//(function () {
+//    var dates = new Date();
+//    for(var i = 0; i<5; i++){
+//        //var nextDay = date.setDate(date.getDate() + 1);
+//        weatherDates.push(dates);
+//        dates = dates.setDate(dates.getDate() + 1);
+//    }
+//    console.log(weatherDates);
+//})();
+
 angular.module('luccaApp').controller('mainCategoryController', function($scope, $http, $resource, GetData, WEATHER_URL){
     $scope.categories = "";
 
@@ -8,7 +19,17 @@ angular.module('luccaApp').controller('mainCategoryController', function($scope,
         $scope.categories = data;
     });
 
-    $scope.date = new Date();
+    //create array of days for weather widget
+    $scope.weatherDates = [];
+    (function () {
+        for(var i = 0; i<5; i++){
+            //var nextDay = date.setDate(date.getDate() + 1);
+
+            var dates = moment().add(i, 'days').format('dddd');
+            $scope.weatherDates.push(dates);
+            //debugger;
+        }
+    })();
 
     $http.get(WEATHER_URL.C).
         success(function(data, status, headers, config) {
