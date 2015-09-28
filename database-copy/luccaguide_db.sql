cac7ebf506ef6ddb314a7c2190b385a742adf390
ObsidianCat <opheliaandcat@gmail.com>
2015-09-28 16:34:28 +0300

    Database updated

--- database-copy/luccaguide_db.sql
+++ database-copy/luccaguide_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 22, 2015 at 12:57 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: Sep 28, 2015 at 03:33 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,12 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `items` (
-`item_id` int(6) NOT NULL,
+  `item_id` int(6) NOT NULL,
   `mainTitle` varchar(200) NOT NULL,
   `subTitle` varchar(200) NOT NULL,
   `address` text NOT NULL,
   `description` text NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `items`
@@ -44,11 +44,31 @@ INSERT INTO `items` (`item_id`, `mainTitle`, `subTitle`, `address`, `description
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `item_images`
+--
+
+CREATE TABLE IF NOT EXISTS `item_images` (
+  `image_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `item_images`
+--
+
+INSERT INTO `item_images` (`image_id`, `item_id`, `image`) VALUES
+(1, 1, ''),
+(2, 1, '');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `reviews`
 --
 
 CREATE TABLE IF NOT EXISTS `reviews` (
-`review_id` int(6) NOT NULL,
+  `review_id` int(6) NOT NULL,
   `author` varchar(200) NOT NULL,
   `country` varchar(100) NOT NULL,
   `dateofreview` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
@@ -57,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   `rating` int(2) NOT NULL,
   `message` text NOT NULL,
   `item_id` int(6) DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reviews`
@@ -65,8 +85,8 @@ CREATE TABLE IF NOT EXISTS `reviews` (
 
 INSERT INTO `reviews` (`review_id`, `author`, `country`, `dateofreview`, `dateofvisit`, `email`, `rating`, `message`, `item_id`) VALUES
 (1, 'Parvati Cat', 'Israel', '2015-08-15 13:33:29', '2015-08-15', 'parvatiCat@example.com', 4, 'Interesting place to visit, reasonable price, lots of history. Good stained glass and always a cool spot to sleep on a hot day.', 1),
-(2, 'CaterinaYorkshire', 'United Kingdom', '2015-08-15 13:36:35', '2015-08-15', 'CaterinaYorkshire@gmail.com', 5, 'message":"Beautiful, not to commercialised, some really interesting sculptures invlipuding a life sized Christ, which is used as part of the September festival every September, only 3? and the money is used to support the restoration.', 1),
-(3, 'Suzbuster', 'USA', '2015-08-15 13:36:44', '2015-08-15', 'suzbuster@gmail.com', 2, 'message":"This duomo doesn''t compare in scale, colour, beauty or grandeur to other duomos I have seen over the past few days (Siena and Pisa). In fact Lucca as a whole did nothing for me and I am quite annoyed at Lonely Planet for putting in Italy''s top 10 destinations.', 1);
+(2, 'CaterinaYorkshire', 'United Kingdom', '2015-08-15 13:36:35', '2015-08-15', 'CaterinaYorkshire@gmail.com', 5, 'Beautiful, not to commercialised, some really interesting sculptures invlipuding a life sized Christ, which is used as part of the September festival every September, only 3? and the money is used to support the restoration.', 1),
+(3, 'Suzbuster', 'USA', '2015-08-15 13:36:44', '2015-08-15', 'suzbuster@gmail.com', 2, 'This duomo doesn''t compare in scale, colour, beauty or grandeur to other duomos I have seen over the past few days (Siena and Pisa). In fact Lucca as a whole did nothing for me and I am quite annoyed at Lonely Planet for putting in Italy''s top 10 destinations.', 1);
 
 --
 -- Indexes for dumped tables
@@ -76,13 +96,19 @@ INSERT INTO `reviews` (`review_id`, `author`, `country`, `dateofreview`, `dateof
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
- ADD PRIMARY KEY (`item_id`);
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `item_images`
+--
+ALTER TABLE `item_images`
+  ADD PRIMARY KEY (`image_id`), ADD KEY `item_id` (`item_id`);
 
 --
 -- Indexes for table `reviews`
 --
 ALTER TABLE `reviews`
- ADD PRIMARY KEY (`review_id`), ADD KEY `item_id` (`item_id`);
+  ADD PRIMARY KEY (`review_id`), ADD KEY `item_id` (`item_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -92,15 +118,26 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-MODIFY `item_id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `item_id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `item_images`
+--
+ALTER TABLE `item_images`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-MODIFY `review_id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `review_id` int(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `item_images`
+--
+ALTER TABLE `item_images`
+ADD CONSTRAINT `item_images_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`);
 
 --
 -- Constraints for table `reviews`
