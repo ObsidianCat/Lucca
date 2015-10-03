271cf4eb1604cdd0e73166453eaa7d2a529a8b79
ObsidianCat <opheliaandcat@gmail.com>
2015-10-03 16:35:35 +0300

    Fix for router params

--- scripts/subCategoryCtrl.js
+++ scripts/subCategoryCtrl.js
@@ -1,13 +1,11 @@
 angular.module('luccaApp').controller('subCategoryController', function($scope, $http, $resource, $routeParams, GetData){
     $scope.subCategoryItems = "";
-    $scope.param = $routeParams.param;
 
-    $scope.subCategoryItems = GetData.returnedData.get({res:$scope.param}).$promise.then(function(data) {
+    $scope.title = $routeParams.param;
+    $scope.pageTitle = $scope.title.charAt(0).toUpperCase() +$scope.title.slice(1);
+    $scope.subCategoryItems = GetData.returnedData.get({res:$scope.title}).$promise.then(function(data) {
         $scope.subCategoryItems = data;
         console.log(data);
         //console.log($scope.subCategoryItems);
     });
-
-    //str = str.replace(/\s/g, "+");
-
 });
