7eebf9248d52687cd584ea1970b5d127876da0ca
ObsidianCat <opheliaandcat@gmail.com>
2015-11-08 14:43:01 +0200

    Work on items CRUD

---
+++ scripts/directives/editItemFormDrct.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Lula on 11/7/2015.
+ */
+angular.module('luccaAdminApp').directive('editItemForm', function() {
+    return {
+        restrict: 'E',
+        templateUrl:'partials/edit-item-form.html'
+    };
+});
